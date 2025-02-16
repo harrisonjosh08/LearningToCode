e8c50693265cfae369c78ade0b957d4ff132f139
Joshua Harrison <harrisonjosh08@gmail.com>
2025-02-16 21:16:28 +1100

    Added Projects

---
+++ Python/inputs.py
@@ -0,0 +1,8 @@
+name = input("what is your name?: ")
+age = int(input("How old are you?: ")) + 1
+
+# age = age + 1
+
+print(f"Hello {name}!")
+print("HAPPY BIRTHDAY!")
+print(f"You are {age} years old!")
---
+++ Python/learning_variables.py
@@ -0,0 +1,27 @@
+# Learnong-Strings
+FIRST_NAME = "Someone"
+FOOD = "pizza"
+EMAIL = "fake@EMAIL.com"
+print(f"Hello {FIRST_NAME}")
+print(f"You like {FOOD}")
+print(f"Your email is: {EMAIL}")
+
+# Integers
+AGE = 25
+QUANTITY = 3
+NUM_OF_STUDENTS = 30
+
+print(f"You are {AGE} years old")
+print(f"You are buying {QUANTITY} items")
+print(f"Your class has {NUM_OF_STUDENTS} students")
+
+# Floats
+PRICE = 10.99
+
+print(f"The price is ${PRICE}")
+
+# Boolean
+
+IS_STUDENT = True
+
+print(f"Are you a student?: {IS_STUDENT}")
