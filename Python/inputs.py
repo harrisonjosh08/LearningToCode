92b49c9da6eeefb53199ef8250e6050a75de2712
harrisonjosh08 <harrisonjosh08@gmail.com>
2025-02-18 23:35:14 +1100

    deleted files

--- Python/inputs.py
+++
@@ -1,8 +0,0 @@
-name = input("what is your name?: ")
-age = int(input("How old are you?: ")) + 1
-
-# age = age + 1
-
-print(f"Hello {name}!")
-print("HAPPY BIRTHDAY!")
-print(f"You are {age} years old!")
--- Python/main.py
+++
@@ -1,6 +0,0 @@
-length = float(input("Enter the length: "))
-width = float(input("Enter the width: "))
-
-area = length * width
-
-print(f"The area is: {area}cm²")
