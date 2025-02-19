2a3d9019df2b02643f4235c4e589fd55a7e76c37
harrisonjosh08 <harrisonjosh08@gmail.com>
2025-02-19 22:48:24 +1100

    added FoodOrdering App tutorial progress

---
+++ React/FoodOrdering/src/components/ProductListItem.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Colors from '../constants/Colors';
+import { Product } from '../types';
+
+export const defaultPizzaImage =
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+  product: Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+}
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: Colors.light.tint,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+});
