86242b4b97309a15d71d455a9fae1ba26332f26c
harrisonjosh08 <harrisonjosh08@gmail.com>
2025-02-19 23:00:37 +1100

    added FoodOrdering App Flatlist

--- React/FoodOrdering/src/app/(tabs)/index.tsx
+++ React/FoodOrdering/src/app/(tabs)/index.tsx
@@ -7,6 +7,8 @@ export default function MenuScreen() {
     <FlatList
       data={products} renderItem={({ item }) => <ProductListItem product={item} />}
       numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
     />
   );
 }
